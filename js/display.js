45d3431b705fb93abbd36a1d1c3bb6a155cbbd24
IBatsios <ioannisbatsios@gmail.com>
2017-06-05 18:10:47 -0400

    Fixed gallery. Added Hollers to image gallery

---
+++ js/display.js
@@ -0,0 +1,98 @@
+function getOCP() {
+    document.getElementById("ocp").style.display = "inline";
+    document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getOutsiders() {
+	document.getElementById("ocp").style.display = "none";
+    document.getElementById("outsiders").style.display = "inline";    
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getResurrection() {
+    document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "inline";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getUTD() {
+    document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "inline";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getBanshee() {
+	document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "inline";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getHollers() {
+	document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "inline";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getDockery() {
+    document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "inline";
+	document.getElementById("lid").style.display = "none";
+}
+
+function getLID() {
+    document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "inline";
+}
+
+function hideAll() {
+	document.getElementById("ocp").style.display = "none";
+	document.getElementById("outsiders").style.display = "none";
+	document.getElementById("resurrection").style.display = "none";
+	document.getElementById("utd").style.display = "none";
+	document.getElementById("banshee").style.display = "none";
+	document.getElementById("hollers").style.display = "none";
+	document.getElementById("dockery").style.display = "none";
+	document.getElementById("lid").style.display = "none";
+}
